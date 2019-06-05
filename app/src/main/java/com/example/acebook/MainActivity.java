7c7a1da9d6c15d20f9af67d998ac595a6587f640
lindsey <lindsey@makers.tech>
2019-06-05 16:48:49 +0100

    make calculator

---
+++ app/src/main/java/com/example/acebook/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.acebook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button submitBtn = (Button) findViewById(R.id.button);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.editText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.editText2);
+                TextView resultTextView = (TextView) findViewById(R.id.textView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+
+                int result = num1 + num2;
+
+                resultTextView.setText(result + "");
+
+            }
+        });
+    }
+
+}
